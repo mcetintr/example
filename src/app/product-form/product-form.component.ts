24b213070563367bcb1eb1728413827d4fdfa35c
mcetintr <mcetintr@gmail.com>
2019-10-29 22:08:54 +0300

    hata verdi

--- src/app/product-form/product-form.component.ts
+++ src/app/product-form/product-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductRepository } from '../repository.model';
 import { Product } from '../product.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'product-form',
@@ -10,14 +11,60 @@ import { Product } from '../product.model';
 export class ProductFormComponent {
 
   model: ProductRepository;
-  newProduct : Product = new Product();
+  newProduct: Product = new Product();
 
-  get jsonProduct(){
+  get jsonProduct() {
     return JSON.stringify(this.newProduct);
   }
 
-  addProduct(p: Product){
+  addProduct(p: Product) {
     console.log("New Product : " + this.jsonProduct);
+  }
+
+  log(m) {
+    console.log(m);
+  }
+
+  formSumitted : boolean = false;
+
+  submitForm(form:NgForm){
+    this.formSumitted = true;
+    if(form.valid){
+      this.addProduct(this.newProduct);
+      this.newProduct = new Product();
+      form.reset;
+      this.formSumitted = false;
+    }
+
+    console.log(form);
+  }
+
+  getValidationErrors(model: any) {
+    let ctrlName: string = model.name;
+    let messages: string[] = [];
+
+    if (model.errors) {
+      //console.log(model.errors);
+      for (const errorName in model.errors) {
+        //console.log(errorName);
+        switch (errorName) {
+          case "required":
+            messages.push(`You must enter a ${ctrlName}`);
+            break;
+          case "minlength":
+            messages.push(`min 3 character for ${ctrlName}`);
+            break;
+          case "pattern":
+            messages.push(`${ctrlName} contains illegal characters.`);
+            break;
+  
+          default:
+            break;
+        }        
+      }
+    }
+   return messages;
+
   }
   constructor() { }
 
